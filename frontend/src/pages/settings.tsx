3069b7361bbc25fc093e11570f1dee06319a9cdd
Raresh Nistor <raresh@nistor.email>
2026-01-23 09:09:17 +1300

    Scaffold out settings pages + Session list

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -1,10 +1,59 @@
+import { ListItem } from "@/components/list-item";
+import { useAuth } from "@/contexts/auth-context";
+import {
+  LucideChevronRight,
+  LucideCircleCheckBig,
+  LucideInfo,
+  LucideLibraryBig,
+  LucideUsers,
+} from "lucide-react";
+import { useNavigate } from "react-router";
+
 export function SettingsPage() {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  const adminSection = auth.user?.admin ? (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-1xl font-semibold">Administration</h1>
+
+      <ListItem
+        title="Manage users"
+        leading={<LucideUsers />}
+        trailing={<LucideChevronRight />}
+        onClick={() => navigate("/settings/users")}
+      />
+      <ListItem
+        title="Manage library"
+        leading={<LucideLibraryBig />}
+        trailing={<LucideChevronRight />}
+        onClick={() => navigate("/settings/library")}
+      />
+      <ListItem
+        title="Background jobs"
+        leading={<LucideCircleCheckBig />}
+        trailing={<LucideChevronRight />}
+        onClick={() => navigate("/settings/jobs")}
+      />
+    </div>
+  ) : (
+    <></>
+  );
+
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold mb-4">Settings</h1>
-      <p className="text-muted-foreground">
-        Configure your application preferences.
-      </p>
+    <div className="p-6 flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold">Settings</h1>
+
+      <div className="flex flex-col gap-2">
+        <ListItem
+          title="Instance information"
+          leading={<LucideInfo />}
+          trailing={<LucideChevronRight />}
+          onClick={() => navigate("/settings/info")}
+        />
+      </div>
+
+      {adminSection}
     </div>
   );
 }
---
+++ frontend/src/pages/settings/manage-users.tsx
@@ -0,0 +1,3 @@
+export function ManageUsersPage() {
+  return <div className="p-6"></div>;
+}
